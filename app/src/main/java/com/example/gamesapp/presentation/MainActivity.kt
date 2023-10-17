eb82ad798e42a0a88f0a69be0e490b7e1250a7d3
Ahmed Mosad <ahmedmooth@gmail.com>
2023-10-17 06:04:51 +0300

    splash screen added

--- app/src/main/java/com/example/gamesapp/presentation/MainActivity.kt
+++ app/src/main/java/com/example/gamesapp/presentation/MainActivity.kt
@@ -16,6 +16,7 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = DataBindingUtil.setContentView(this,R.layout.activity_main)
+        setTheme(R.style.AppTheme)
         setSupportActionBar(binding.toolbar)
         supportActionBar?.setDisplayHomeAsUpEnabled(true)
 
